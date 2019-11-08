a0898058056bb73dd6a2991db088bebcf849c9c0
MadeWilliam <pupul.mwm@gmail.com>
2019-11-09 06:23:08 +0700

    passing information

--- ReactJS/ravenous/src/components/BusinessList/BusinessList.js
+++ ReactJS/ravenous/src/components/BusinessList/BusinessList.js
@@ -3,16 +3,22 @@ import "./BusinessList.css";
 import Business from "../Business/Business";
 
 class BusinessList extends Component {
-    sixBusiness() {
-        let business = [];
-        for (let index = 0; index < 6; index++) {
-            business[index] = <Business />;
-        }
-        return business;
-    }
+    // sixBusiness() {
+    //     let business = [];
+    //     for (let index = 0; index < 6; index++) {
+    //         business[index] = <Business />;
+    //     }
+    //     return business;
+    // }
 
     render() {
-        return <div className="BusinessList">{this.sixBusiness()}</div>;
+        return (
+            <div className="BusinessList">
+                {this.props.businesses.map(business => {
+                   return <Business business={business} />;
+                })}
+            </div>
+        );
     }
 }
 
