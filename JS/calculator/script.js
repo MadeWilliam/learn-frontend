1690e5a6a19d3d7bbb696df3fb77129915cea1aa
MadeWilliam <pupul.mwm@gmail.com>
2019-11-14 16:02:12 +0700

    add function stage-1

--- JS/calculator/script.js
+++ JS/calculator/script.js
@@ -1,7 +1,20 @@
+const calculate = (n1, operator, n2) => {
+  let result = "";
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+  return result;
+};
+
 const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".calculator__display span");
 const keys = calculator.querySelector(".calculator__keys");
-const display = document.querySelector(".calculator__display span");
-let decimalFlag = false;
 
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
@@ -11,6 +24,22 @@ keys.addEventListener("click", e => {
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
 
+    Array.from(key.parentNode.children).forEach(k =>
+      k.classList.remove("is-depressed")
+    );
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent += keyContent;
+      }
+    }
+
+    if (action === "decimal") {
+      display.textContent += ".";
+    }
+
     if (
       action === "add" ||
       action === "subtract" ||
@@ -18,49 +47,9 @@ keys.addEventListener("click", e => {
       action === "divide"
     ) {
       key.classList.add("is-depressed");
-      // Add custom attribute
       calculator.dataset.previousKeyType = "operator";
-    }
-
-    if (!action) {
-      if (displayedNum === "0" || previousKeyType === "operator") {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
-    }
-
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children).forEach(k =>
-      k.classList.remove("is-depressed")
-    );
-
-    // if (!action) {
-    //   if (displayedNum === "0") {
-    //     display.textContent = keyContent;
-    //   } else {
-    //     display.textContent = displayedNum + keyContent;
-    //   }
-    // }
-
-    if (action === "decimal" && !decimalFlag) {
-      display.textContent = displayedNum + ".";
-      decimalFlag = true;
-    }
-
-    if (!action) {
-      console.log("number key!");
-    }
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "multiply" ||
-      action === "divide"
-    ) {
-      console.log("operator key!");
-    }
-    if (action === "decimal") {
-      console.log("decimal key!");
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
 
     if (action === "clear") {
@@ -68,57 +57,11 @@ keys.addEventListener("click", e => {
     }
 
     if (action === "calculate") {
-      console.log("equal key!");
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
-
-// const CALCULATOR = document.querySelector(".calculator");
-// const KEYS = CALCULATOR.querySelector(".calculator__keys");
-// const DISPLAY = document.querySelector("calculator__display");
-
-// KEYS.addEventListener("click", e => {
-//   if (e.target.matches("button")) {
-//     const KEY = e.target;
-//     const ACTION = KEY.dataset.action;
-//     const KEY_CONTENT = KEY.textContent;
-//     const DISPLAYED_NUM = DISPLAY.textContent;
-
-//     if (!ACTION) {
-//       if (DISPLAYED_NUM === "0") {
-//         DISPLAY.textContent = KEY_CONTENT;
-//       }
-//     }
-
-//     if (!ACTION) {
-//       if (DISPLAYED_NUM === '0') {
-//         DISPLAY.textContent = KEY_CONTENT
-//       } else {
-//         DISPLAY.textContent = DISPLAYED_NUM + KEY_CONTENT
-//       }
-//     }
-
-//     if (!ACTION) {
-//       console.log("number key!");
-//     }
-//     if (
-//       ACTION === "add" ||
-//       ACTION === "subtract" ||
-//       ACTION === "multiply" ||
-//       ACTION === "divide"
-//     ) {
-//       console.log("operator key!");
-//     }
-//     if (ACTION === "decimal") {
-//       console.log("decimal key!");
-//     }
-
-//     if (ACTION === "clear") {
-//       console.log("clear key!");
-//     }
-
-//     if (ACTION === "calculate") {
-//       console.log("equal key!");
-//     }
-//   }
-// });
