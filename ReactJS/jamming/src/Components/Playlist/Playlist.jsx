1dea7ea0133df76b586ad7ebc52b94ea961bb905
MadeWilliam <pupul.mwm@gmail.com>
2019-12-02 17:39:26 +0700

    Pass isRemoval with a value of false down to TrackList

---
+++ ReactJS/jamming/src/Components/Playlist/Playlist.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import TrackList from '../TrackList/TrackList.jsx'
+
+export class Playlist extends Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={"New Playlist"} />
+        <TrackList tracks={this.props.playlistTracks}/>
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
